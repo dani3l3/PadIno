b906308fa4808e211a0e2ccc08737fe4d9bf22ba
Daniele Muscetta <daniele@muscetta.com>
2025-01-05 10:51:10 +0100

    code refers to readme in repo to reduce duplication of text

--- PadIno.ino
+++ PadIno.ino
@@ -1,29 +1,12 @@
 /*
 
-'Pad-ino' MIDI 6 Drum Pads Controller by Plank guitars and musical things by Shambien (Daniele Muscetta) - https://www.shambien.com
+'Pad-ino' 
+a MIDI 6 Drum Pads Controller by Plank guitars and musical things by Shambien (Daniele Muscetta) - https://www.shambien.com
 
-2025 01 04 - v.01 - initial version (still needs some tweaking of thresholds for sensitivity and some code/comments cleanup. Also I tried incorporating the USB-Rename library but it still doesn't work so it's commented out, TBD.)
+2025 01 04 - v.01 - initial version.
 
-Works on Arduino Pro Micro / Leonardo - UNTESTED on any other board (but it should work on all those based on the Atmega32U4 chipset).
+For more information and credits refer to ReadMe in the GitHub repository https://github.com/dani3l3/PadIno
 
-Besides the code, in this repository you can find [several pictures I took during various phases of the build process](./img/).
-
-Contains portions of code, inspiration and teachings derived from various sources such as: 
-- Arduino MIDIUSB Library https://github.com/arduino-libraries/MIDIUSB
-- Responsive Analog Read library https://github.com/dxinteractive/ResponsiveAnalogRead
-- Simple DYI Electronic Music Projects https://github.com/diyelectromusic/sdemp
-- Nerd Musician (Gustavo Silveira) https://github.com/silveirago and https://www.youtube.com/@NerdMusician
-- BeginnerToPro https://www.youtube.com/@BeginnerTopro https://www.youtube.com/watch?v=5SL-W1Ynn3c (the hardware circuit I built - piezo's, resistors and diodes - is based on this video's circuit, even if it was for an 'octapad' and mine has only six pads)
-- Autonomia Digital https://www.youtube.com/watch?v=sU0ZmjipUU0 https://www.youtube.com/@autonomiadigital9664
-
-...and many other sources I consulted and learned from but I did not bookmark (but you might find them by searching for keywords such as 'octapad piezo arduino' on YouTube).
-
-I cannot thank enough all of the authors of the above for teaching me thru their tutorials, experiences and code! 
-May God bless you always.
-
-Keep grooving!
-
-Shambien
 
 */
 
